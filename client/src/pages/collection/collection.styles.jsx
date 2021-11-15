d938a3389073ae495530984b7c9f3fad5ca17e7e
PhilLaptop <philngtn@gmail.com>
2021-11-15 00:08:27 -0500

    Fixing the layout of the collection page

--- client/src/pages/collection/collection.styles.jsx
+++ client/src/pages/collection/collection.styles.jsx
@@ -3,10 +3,11 @@ import styled from "styled-components";
 export const CollectionPageContainer = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
 `;
 
 export const CollectionTitle = styled.h2`
-  font-size: 38px;
+  font-size: 40px;
   margin: 0 auto 30px;
 `;
 
@@ -21,6 +22,6 @@ export const CollectionItemsContainer = styled.div`
   @media screen and (max-width: 800px) {
     display: grid;
     grid-template-columns: 1fr 1fr;
-    grid-gap: 15px;
+    grid-gap: 20px;
   }
 `;
